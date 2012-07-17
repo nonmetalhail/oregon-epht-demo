027b1fcd12aaeb4d5156381d36dd26c32fa39b14
nahman@google.com <nahman@google.com@5ec33184-6660-f1e0-0350-be708ec807de>
2012-07-17 21:29:47 +0000

    basic form for the or site

---
+++ epht_demo.js
@@ -0,0 +1,156 @@
+var data_sets = {
+  'asthma':{
+    'All':{
+      'tid':'',
+      'table': "https://www.google.com/fusiontables/embedviz?viz=GVIZ&t=TABLE&containerId=gviz_canvas&q=select+col0%3E%3E0%2C+col9%3E%3E0+from+1r9i-iy46IDJfUcETQwP6v4W6dXaFJYEonNTlQbk+where+col0%3E%3E0+in+('Iowa'%2C+'Ohio'%2C+'Utah'%2C+'Idaho'%2C+'Maine'%2C+'Texas'%2C+'Kentucky'%2C+'Maryland'%2C+'Washington'%2C+'Tennessee'%2C+'North+Dakota'%2C+'South+Dakota'%2C+'Alabama'%2C+'Nebraska'%2C+'Delaware'%2C+'Florida'%2C+'Arizona'%2C+'Pennsylvania'%2C+'New+York'%2C+'California'%2C+'New+Jersey'%2C+'New+Mexico'%2C+'Georgia'%2C+'Virginia'%2C+'South+Carolina'%2C+'Wisconsin'%2C+'Alaska'%2C+'Vermont'%2C+'Arkansas'%2C+'Colorado'%2C+'Connecticut'%2C+'District+of+Columbia'%2C+'Hawaii'%2C+'Illinois'%2C+'Indiana'%2C+'Kansas'%2C+'Louisiana'%2C+'Massachusetts'%2C+'Michigan'%2C+'Minnesota'%2C+'Mississippi'%2C+'Missouri'%2C+'Montana'%2C+'Nevada'%2C+'New+Hampshire'%2C+'North+Carolina'%2C+'Oklahoma'%2C+'Oregon'%2C+'Rhode+Island'%2C+'West+Virginia'%2C+'Wyoming')",
+      'map': 'https://www.google.com/fusiontables/embedviz?viz=MAP&q=select+col3%3E%3E1+from+1r9i-iy46IDJfUcETQwP6v4W6dXaFJYEonNTlQbk&h=false&lat=37.0&lng=-95.0&z=3&t=1&l=col3%3E%3E1&y=1&tmplt=3',
+      'chart': 'https://www.google.com/fusiontables/embedviz?viz=GVIZ&t=COLUMN&containerId=gviz_canvas&q=select+col0%2C+col2%2C+col1+from+1bx4Fwbvf6A9mZukjhaMBe8drtPt2dTWVvmXtH4Y&qrs=+where+col0+%3E%3D+&qre=+and+col0+%3C%3D+&qe=+limit+4&gco_vAxes=%5B%7B%22title%22%3A%22%C2%A2%2FkWh%22%2C%22minValue%22%3A0%2C%22maxValue%22%3Anull%2C%22viewWindowMode%22%3A%22pretty%22%2C%22viewWindow%22%3A%7B%22max%22%3Anull%2C%22min%22%3A0%7D%2C%22useFormatFromData%22%3Atrue%2C%22gridlines%22%3A%7B%22count%22%3A%2210%22%2C%22color%22%3A%22%23d9d9d9%22%7D%7D%2C%7B%22viewWindowMode%22%3A%22pretty%22%2C%22viewWindow%22%3A%7B%7D%2C%22useFormatFromData%22%3Atrue%7D%5D&width=350&height=200'
+    },
+    'Male': {
+      'tid':'',
+      'table': "https://www.google.com/fusiontables/embedviz?viz=GVIZ&t=TABLE&containerId=gviz_canvas&q=select+col0%3E%3E0%2C+col9%3E%3E0+from+1r9i-iy46IDJfUcETQwP6v4W6dXaFJYEonNTlQbk+where+col0%3E%3E0+in+('Iowa'%2C+'Ohio'%2C+'Utah'%2C+'Idaho'%2C+'Maine'%2C+'Texas'%2C+'Kentucky'%2C+'Maryland'%2C+'Washington'%2C+'Tennessee'%2C+'North+Dakota'%2C+'South+Dakota'%2C+'Alabama'%2C+'Nebraska'%2C+'Delaware'%2C+'Florida'%2C+'Arizona'%2C+'Pennsylvania'%2C+'New+York'%2C+'California'%2C+'New+Jersey'%2C+'New+Mexico'%2C+'Georgia'%2C+'Virginia'%2C+'South+Carolina'%2C+'Wisconsin'%2C+'Alaska'%2C+'Vermont'%2C+'Arkansas'%2C+'Colorado'%2C+'Connecticut'%2C+'District+of+Columbia'%2C+'Hawaii'%2C+'Illinois'%2C+'Indiana'%2C+'Kansas'%2C+'Louisiana'%2C+'Massachusetts'%2C+'Michigan'%2C+'Minnesota'%2C+'Mississippi'%2C+'Missouri'%2C+'Montana'%2C+'Nevada'%2C+'New+Hampshire'%2C+'North+Carolina'%2C+'Oklahoma'%2C+'Oregon'%2C+'Rhode+Island'%2C+'West+Virginia'%2C+'Wyoming')",
+      'map': 'https://www.google.com/fusiontables/embedviz?viz=MAP&q=select+col3%3E%3E1+from+1r9i-iy46IDJfUcETQwP6v4W6dXaFJYEonNTlQbk&h=false&lat=37.0&lng=-95.0&z=3&t=1&l=col3%3E%3E1&y=2&tmplt=1',
+      'chart': 'https://www.google.com/fusiontables/embedviz?viz=GVIZ&t=COLUMN&containerId=gviz_canvas&q=select+col0%2C+col2%2C+col1+from+1bx4Fwbvf6A9mZukjhaMBe8drtPt2dTWVvmXtH4Y&qrs=+where+col0+%3E%3D+&qre=+and+col0+%3C%3D+&qe=+limit+4&gco_vAxes=%5B%7B%22title%22%3A%22%C2%A2%2FkWh%22%2C%22minValue%22%3A0%2C%22maxValue%22%3Anull%2C%22viewWindowMode%22%3A%22pretty%22%2C%22viewWindow%22%3A%7B%22max%22%3Anull%2C%22min%22%3A0%7D%2C%22useFormatFromData%22%3Atrue%2C%22gridlines%22%3A%7B%22count%22%3A%2210%22%2C%22color%22%3A%22%23d9d9d9%22%7D%7D%2C%7B%22viewWindowMode%22%3A%22pretty%22%2C%22viewWindow%22%3A%7B%7D%2C%22useFormatFromData%22%3Atrue%7D%5D&width=350&height=200'
+    },
+    'Female': {
+      'tid':'',
+      'table': '',
+      'map': '',
+      'chart': ''
+    }
+  },
+  'cancer':{
+    'All':{
+      'tid':'',
+      'table': '',
+      'map': '',
+      'chart': '' 
+    },
+    'Bladder':{
+      'tid':'',
+      'table': '',
+      'map': '',
+      'chart': ''
+    },
+    'Brain & CNS':{
+      'tid':'',
+      'table': '',
+      'map': '',
+      'chart': ''
+    },
+    'Breast':{
+      'tid':'',
+      'table': '',
+      'map': '',
+      'chart': ''
+    },
+    'Breast < 50yr':{
+      'tid':'',
+      'table': '',
+      'map': '',
+      'chart': ''
+    },
+    'Breast >=50yr':{
+      'tid':'',
+      'table': '',
+      'map': '',
+      'chart': ''
+    },
+    'Leukemia':{
+      'tid':'',
+      'table': '',
+      'map': '',
+      'chart': ''
+    },
+    'Leukemia-Lymphocytic':{
+      'tid':'',
+      'table': '',
+      'map': '',
+      'chart': ''
+    },
+    'Leukemia-Myeloid':{
+      'tid':'',
+      'table': '',
+      'map': '',
+      'chart': ''
+    },
+    'Lung & Bronchus':{
+      'tid':'',
+      'table': '',
+      'map': '',
+      'chart': ''
+    },
+    'Melanoma':{
+      'tid':'',
+      'table': '',
+      'map': '',
+      'chart': ''
+    },
+    'Mesotheloma':{
+      'tid':'',
+      'table': '',
+      'map': '',
+      'chart': ''
+    },
+    "Non-Hodgkin's Lymphoma":{
+      'tid':'',
+      'table': '',
+      'map': '',
+      'chart': ''
+    },
+    'Thyroid':{
+      'tid':'',
+      'table': '',
+      'map': '',
+      'chart': ''
+    }
+  },
+  'co':{
+    'All':{
+      'tid':'',
+      'table': '',
+      'map': '',
+      'chart': ''
+    }
+  },
+  'heart_attack':{
+    'All':{
+      'tid':'',
+      'table': '',
+      'map': '',
+      'chart': ''
+    }
+  },
+  'reproductive':{
+    'All':{
+      'tid':'',
+      'table': '',
+      'map': '',
+      'chart': ''
+    }
+  }
+};
+
+$(document).ready(function(){
+  $('#disease_sets').live('change',function(){
+    $('#data_sets').children().remove();
+    for(var item in data_sets[this.value]){
+      $('#data_sets').append('<option value = "'+
+        item+'">'+item+'</option>');
+    }
+    $('#data_sets option[value="All"]').trigger('change');
+  });
+
+  $('#data_sets').live('change',function(){
+    var disease = $('#disease_sets option:selected').attr('value');
+    var dataset = this.value;
+    console.log(disease);
+    console.log(dataset);
+    console.log(data_sets[disease][dataset]);
+    $('#map').attr('src',data_sets[disease][dataset]['map']);
+    $('#graph').attr('src',data_sets[disease][dataset]['chart']);
+    $('#table').attr('src',data_sets[disease][dataset]['table']);
+  });
+
+  $('#disease_sets').trigger('change');
+});
