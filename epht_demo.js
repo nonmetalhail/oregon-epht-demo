2070eb31c3021407467e30091a37caf17eaf83d5
nahman@google.com <nahman@google.com@5ec33184-6660-f1e0-0350-be708ec807de>
2012-08-02 18:41:14 +0000

    realized I completely forgot that they would have to put embedd links for EACH YEAR: got rid of Describe call for years, restrutured json datastructure to account for chart and map for each year, rewrote update code to account on this new data format

--- epht_demo.js
+++ epht_demo.js
@@ -1,50 +1,59 @@
-var json_file_location = 'json/epht.json'
+// these two vars will need to be updated user to user
+var json_file_location = 'json/epht.json';
+var user_fusion_table_api_key = 'AIzaSyA7_yvmF6Aj0z9ctqiVVS5BI9cVIqx7F1w';
 
+// datastructure where the json will get loaded
 var data_sets = {};
-
+/* this is to create the bar chart; 
+   once chart configuration is implemented in FT, then you dont 
+   need this. Instead, put embedd links into the json file.
+*/
 var chartConfig = function(tid,year){
   return "https://www.google.com/fusiontables/embedviz?&containerId=gviz_canvas&viz=GVIZ&q=select%20'Geography'%2C%20'"+year+"'%20from%20"+tid+"%20order%20by%20'"+year+"'%20DESC%20limit%20100&t=BAR&gco_width=500&gco_height=400&gco_chartArea=%7B%22height%22%3A%22350%22%2C%22top%22%3A%2220%22%7D&gco_hAxis=%7B%22viewWindowMode%22%3A%22pretty%22%2C%22viewWindow%22%3A%7B%7D%2C%22useFormatFromData%22%3Atrue%2C%22format%22%3A%22%22%7D&gco_vAxis=%7B%22format%22%3A%22%22%7D&gco_vAxes=%5B%7B%22title%22%3Anull%2C%22minValue%22%3Anull%2C%22maxValue%22%3Anull%2C%22viewWindowMode%22%3A%22pretty%22%2C%22viewWindow%22%3A%7B%22max%22%3Anull%2C%22min%22%3Anull%7D%2C%22useFormatFromData%22%3Atrue%2C%22textStyle%22%3A%7B%22color%22%3A%22%23222%22%2C%22fontSize%22%3A%2208%22%7D%7D%2C%7B%22viewWindowMode%22%3A%22pretty%22%2C%22viewWindow%22%3A%7B%7D%2C%22useFormatFromData%22%3Atrue%7D%5D&gco_curveType=&gco_animation=%7B%22duration%22%3A500%7D&gco_booleanRole=certainty&gco_lineWidth=2&gco_series=%7B%220%22%3A%7B%22hasAnnotations%22%3Atrue%7D%7D&gco_legend=inside&gco_strictFirstColumnType=false"
 }
 
 $(document).ready(function(){
+  // loads the json file; when done, loads the datasets into the select menu
   $.when(getFTLinks()).done(function(){
     console.log(data_sets)
     for(var item in data_sets){
       $('#disease_sets').append('<option value = "'+
           item+'">'+item+'</option>');
     }
-
+    // set up a listener on the disease select menu
     $('#disease_sets').live('change',function(){
+      // load the sub-sets
       $('#data_sets').children().remove();
       for(var item in data_sets[this.value]){
         $('#data_sets').append('<option value = "'+
           item+'">'+item+'</option>');
       }
-      // $('#data_sets option[value="All"]').trigger('change');
       
-      getYears(this);
-      
-      // var tempYears = data_sets[this.value][$('#data_sets').attr('value')]['years'];
-      // $('#years').children().remove();
-      // for(var i in tempYears){
-      //   $('#years').append('<option value = "'+
-      //     tempYears[i]+'">'+tempYears[i]+'</option>');
-      // }
-      // $('#years option[value="2006"]').trigger('change');
+      var tempYears = data_sets[this.value][$('#data_sets').attr('value')]['years'];
+      $('#years').children().remove();
+      var temp = [];
+      for(var year in tempYears){
+        $('#years').append('<option value = "'+
+          year+'">'+year+'</option>');
+        temp.push(year);
+      }
+      temp.sort().reverse();
+      $('#years option[value="'+temp[0]+'"]').trigger('change');
     });
-
+    // add listener to sub-sets
     $('#data_sets').live('change',function(){
       updateCharts();
     });
-
+    // add listener to years
     $('#years').live('change',function(){
       updateCharts();
     });
-
+    // trigger to load an initial set
     $('#disease_sets').trigger('change');
   });
 });
 
+// function to load the json file
 function getFTLinks(){
   var d = $.Deferred();
   $.getJSON(json_file_location,function(resp){
@@ -56,40 +65,19 @@ function getFTLinks(){
   return d.promise();
 }
 
-function getYears(self){
-  var FTURL = 'https://www.googleapis.com/fusiontables/v1/tables/';
-  var tid = data_sets[self.value][$('#data_sets').attr('value')]['tid'];
-  var key = '?key=AIzaSyA7_yvmF6Aj0z9ctqiVVS5BI9cVIqx7F1w';
-  $.getJSON(FTURL+tid+key,function(resp){
-    var tempYears = [];
-    for(var i in resp['columns']){
-      if(resp['columns'][i]["type"]=="NUMBER"){
-        tempYears.push(resp['columns'][i]["name"]);
-      }
-      else{
-        console.log("rejected col: " + resp['columns'][i]["name"]);
-      }
-    }
-    tempYears.sort().reverse();
-    $('#years').children().remove();
-    for(var i in tempYears){
-      $('#years').append('<option value = "'+
-        tempYears[i]+'">'+tempYears[i]+'</option>');
-    }
-    $('#years option[value="'+tempYears[0]+'"]').trigger('change');
-  });
-}
-
+// function to update the charts on change
 function updateCharts(){
   var disease = $('#disease_sets option:selected').attr('value');
   var dataset = $('#data_sets option:selected').attr('value');
   var year = $('#years option:selected').attr('value');
 
   var data = data_sets[disease][dataset];
-  console.log(disease);
-  console.log(dataset);
-  console.log(data);
-  $('#map').attr('src',data['map']);
+  // console.log(disease);
+  // console.log(dataset);
+  // console.log(data);
+  $('#map').attr('src',data['years'][year]['map']);
+  // use when chart embedds are in json
+  // $('#graph').attr('src',data['years'][year]['chart']);
   $('#graph').attr('src',chartConfig(data['tid'],year));
   $('#table').attr('src',data['table']);
   $('#title').text(data['title']);
